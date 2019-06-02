ff4b52f04c3e86530f904a911b82fae249896b2e
St Kol <st.kolodych@gmail.com>
2019-06-02 21:13:45 +0300

    fetch and add post (firebase)

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,10 +2,13 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput
       control-type="textarea"
       v-model="editedPost.content">Content</AppControlInput>
+      <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">Preview Text</AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton
       type="button"
@@ -37,7 +40,8 @@ export default {
         : {
             author: "",
             title: "",
-            thumbnailLink: "",
+            thumbnail: "",
+            previewText: "",
             content: ""
           }
     };
@@ -45,7 +49,7 @@ export default {
   methods: {
     onSave() {
       // Save the post
-      console.log(this.editedPost);
+      this.$emit('submit', this.editedPost);
     },
     onCancel() {
       // Navigate back
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
   layout: 'admin',
+  methods: {
+    onSubmitted(postData) {
+      axios.post('https://nuxt-blog-21525.firebaseio.com/posts.json', postData)
+      .then(result => console.log(result))
+      .catch(e => console.log(e))
+    }
+  },
   components: {
     AdminPostForm
   }
